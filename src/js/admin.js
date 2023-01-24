717f69a9025419e95acfa7034b0c941379de872a
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-01-24 16:50:22 +0100

    Make load data function from API for admin

--- src/js/admin.js
+++ src/js/admin.js
@@ -2,4 +2,42 @@ import './../css/admin.css';
 
 import ExcursionsAPI from './ExcursionsAPI';
 
-console.log('admin');
+const api = new ExcursionsAPI('excursions');
+const ul = document.querySelector('.panel__excursions');
+const proto = document.querySelector('.excursions__item--prototype');
+
+const init = () => {
+	load();
+};
+
+const load = () => {
+	api.loadData().then((data) => {
+		insertData(data);
+	});
+};
+
+const insertData = (excArray) => {
+	ul.innerHTML = '';
+	excArray.forEach((el) => {
+		const newLiItem = proto.cloneNode(true);
+		newLiItem.classList.remove('excursions__item--prototype');
+		const h2 = newLiItem.firstElementChild.firstElementChild;
+		const p = newLiItem.firstElementChild.lastElementChild;
+		const adultPrice = newLiItem.querySelector(
+			'.excursions__field-price--adult'
+		);
+		const childPrice = newLiItem.querySelector(
+			'.excursions__field-price--child'
+		);
+
+		h2.innerText = el.name;
+		p.innerText = el.description;
+		adultPrice.innerText = el.adultPrice;
+		childPrice.innerText = el.childPrice;
+		newLiItem.dataset.id = el.id;
+
+		ul.appendChild(newLiItem);
+	});
+};
+
+document.addEventListener('DOMContentLoaded', init);
