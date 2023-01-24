68a239b653c44b0119e7e9f6feebe9931e450f99
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-01-24 15:09:40 +0100

    Make loadData function in API class

--- src/js/ExcursionsAPI.js
+++ src/js/ExcursionsAPI.js
@@ -1,5 +1,21 @@
 class ExcursionsAPI {
+	constructor(resource) {
+		this.API_LINK = `http://localhost:3000/${resource}`;
+	}
 
+	loadData() {
+		return this._fetch();
+	}
+
+	_fetch(options, additionalPath = '') {
+		const API_URL = `${this.API_LINK}${additionalPath}`;
+		return fetch(API_URL, options).then((resp) => {
+			if (resp.ok) {
+				return resp.json();
+			}
+			return Promise.reject(resp);
+		});
+	}
 }
 
 export default ExcursionsAPI;
