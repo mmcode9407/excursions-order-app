daebb38a4f6ba5341a9653ee9db07e88acbe525b
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-01-25 10:38:21 +0100

    Refactor load function

--- src/js/admin.js
+++ src/js/admin.js
@@ -11,33 +11,38 @@ const init = () => {
 };
 
 const load = () => {
-	api.loadData().then((data) => {
-		insertData(data);
-	});
+	api
+		.loadData()
+		.then((data) => {
+			insertData(data);
+		})
+		.catch((err) => console.log(err));
 };
 
 const insertData = (excArray) => {
 	ul.innerHTML = '';
-	excArray.forEach((el) => {
-		const newLiItem = proto.cloneNode(true);
-		newLiItem.classList.remove('excursions__item--prototype');
-		const h2 = newLiItem.firstElementChild.firstElementChild;
-		const p = newLiItem.firstElementChild.lastElementChild;
-		const adultPrice = newLiItem.querySelector(
-			'.excursions__field-price--adult'
-		);
-		const childPrice = newLiItem.querySelector(
-			'.excursions__field-price--child'
-		);
-
-		h2.innerText = el.name;
-		p.innerText = el.description;
-		adultPrice.innerText = el.adultPrice;
-		childPrice.innerText = el.childPrice;
-		newLiItem.dataset.id = el.id;
+	excArray.forEach((item) => {
+		const newLiItem = createListEl(item);
 
 		ul.appendChild(newLiItem);
 	});
+};
+
+const createListEl = (itemData) => {
+	const newLiItem = proto.cloneNode(true);
+	newLiItem.classList.remove('excursions__item--prototype');
+	const h2 = newLiItem.firstElementChild.firstElementChild;
+	const p = newLiItem.firstElementChild.lastElementChild;
+	const adultPrice = newLiItem.querySelector('.excursions__field-price--adult');
+	const childPrice = newLiItem.querySelector('.excursions__field-price--child');
+
+	h2.innerText = itemData.name;
+	p.innerText = itemData.description;
+	adultPrice.innerText = itemData.adultPrice;
+	childPrice.innerText = itemData.childPrice;
+	newLiItem.dataset.id = itemData.id;
+
+	return newLiItem;
 };
 
 document.addEventListener('DOMContentLoaded', init);
