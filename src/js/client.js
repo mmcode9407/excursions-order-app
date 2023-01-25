30bd42c87b5ed9fa4b5387a08c557896f854df9b
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-01-25 12:36:57 +0100

    Add load data function from API for client

--- src/js/client.js
+++ src/js/client.js
@@ -2,4 +2,67 @@ import './../css/client.css';
 
 import ExcursionsAPI from './ExcursionsAPI';
 
-console.log('client');
+const api = new ExcursionsAPI('excursions');
+const proto = document.querySelector('.excursions__item--prototype');
+
+const init = () => {
+	load();
+};
+
+const load = () => {
+	api
+		.loadData()
+		.then((data) => {
+			insertData(data);
+		})
+		.catch((err) => console.log(err));
+};
+
+const findRootElement = () => {
+	return document.querySelector('.panel__excursions');
+};
+
+const insertData = (excArray) => {
+	const ulElement = findRootElement();
+	clearElement(ulElement);
+	excArray.forEach((item) => {
+		const newLiItem = createListEl(item);
+
+		ulElement.appendChild(newLiItem);
+	});
+};
+
+const createListEl = (itemData) => {
+	const newLiItem = createElementFromProto();
+	const [title, description, adultPrice, childPrice] = getLiItems(newLiItem);
+
+	title.innerText = itemData.name;
+	description.innerText = itemData.description;
+	adultPrice.innerText = itemData.adultPrice;
+	childPrice.innerText = itemData.childPrice;
+	newLiItem.dataset.id = itemData.id;
+
+	return newLiItem;
+};
+
+const createElementFromProto = () => {
+	const newElement = proto.cloneNode(true);
+	newElement.classList.remove('excursions__item--prototype');
+
+	return newElement;
+};
+
+const getLiItems = (root) => {
+	const title = root.querySelector('.excursions__title');
+	const description = root.querySelector('.excursions__description');
+	const adultPrice = root.querySelector('.excursions__price--adult');
+	const childPrice = root.querySelector('.excursions__price--child');
+
+	return [title, description, adultPrice, childPrice];
+};
+
+const clearElement = (element) => {
+	element.innerHTML = '';
+};
+
+document.addEventListener('DOMContentLoaded', init);
